516ea0c25d7b98526f4eb02a114c8ad51e83f7e5
PoringKing <gryanjeff@yahoo.com>
2024-06-02 22:15:49 -0700

    Create skill.c

---
+++ Paladin Defender MOD/src/map/skill.c
@@ -0,0 +1,7 @@
+case PA_DEFENDER:
+    // Other skill effects...
+    if (sd) {
+        // Reduce movement speed
+        //pc_set_walkspeed(sd, sd->status.walkspd - 50); // Example: Reducing speed
+    }
+    break;
